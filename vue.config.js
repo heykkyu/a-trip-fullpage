4dea29b1ac90b616e1c97537ef1772d01b255c1d
heykkyu <heykkyu@gmail.com>
2020-01-29 15:29:25 +0900

    vue config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,35 @@ module.exports = {
     ? '/trip-fullpage/'
     : '/',
   outputDir: 'docs',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'trip-fullpage',
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+          return options
+      })
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => {
+          options.limit = -1
+          return options
+      })
+  },
   lintOnSave: false,
 }
