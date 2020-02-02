3707e2ea9577b20605c6ee8ba8ddd05f73574035
heykkyu <heykkyu@gmail.com>
2020-02-03 00:53:49 +0900

    page3 contenteeditable

--- src/components/pages/Page3.vue
+++ src/components/pages/Page3.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="TripPage3">
-    <p>how about type what you wanna do?</p>
+    <p
+      contenteditable
+      spellcheck="false" 
+      v-html="example" 
+      ref="example" 
+      @click="example = 'type here!'"></p>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      example: 'how about type what you wanna do?'
+    }
+  }
+}
+</script>
