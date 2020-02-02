ac99f6dcd6259554b6cfed548c7db13d373cc4bf
heykkyu <heykkyu@gmail.com>
2020-02-02 16:28:28 +0900

    video test

--- src/components/pages/Page1.vue
+++ src/components/pages/Page1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="TripPage1">
       <p>Where you wanna go?</p>
-      <video id="myVideo" loop muted data-autoplay>
+      <video id="myVideo" loop muted preload="auto" controls autoplay>>
         <source src="../../assets/main.mp4" type="video/mp4">
       </video>
   </div>
